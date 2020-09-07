0f9089966841863513193a1243ccb9a106db298d
Shad0wMazt3r <58432267+Shad0wMazt3r@users.noreply.github.com>
2020-09-07 13:37:25 +0530

    Create easy-backup.py

---
+++ easy-backup.py
@@ -0,0 +1,15 @@
+# Easy Backup program 
+# Author: Pratyaksha Beri
+# v0.5
+import os
+items = os.listdir()
+items.remove('main.py')
+print(f"backing up {items}")
+os.system("mkdir backup")
+os.chdir("backup")
+for item in items:
+	command = "mkdir "+ item
+	os.system(command)
+	command2 = "cp "+ item +" backup/"+item
+os.system("zip -r backup.zip backup/")
+os.system("rm -r backup")
